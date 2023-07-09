9fb7dcab9fa4b37b0193ae2706afcff494ff947d
rajeshdeshmukh <99680719+rajeshdeshmukh@users.noreply.github.com>
2023-07-10 04:46:33 +0530

    Add files via upload

---
+++ Plastoshield/main.js
@@ -0,0 +1,60 @@
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+
+
+// new----------
+
+window.onload = function(){
+	var getNavi = document.getElementById('navigation');
+
+	var mobile = document.createElement("span");
+	mobile.setAttribute("id","mobile-navigation");
+	getNavi.parentNode.insertBefore(mobile,getNavi);
+
+	document.getElementById('mobile-navigation').onclick = function(){
+		var a = getNavi.getAttribute('style');
+		if(a){
+			getNavi.removeAttribute('style');
+			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-menu.png)';
+		} else {
+			getNavi.style.display='block';
+			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-close.png)';
+		}
+	};
+	var getElm = getNavi.getElementsByTagName("LI");
+	for(var i=0;i<getElm.length;i++){
+		if(getElm[i].children.length>1){
+			var smenu = document.createElement("span");
+			smenu.setAttribute("class","mobile-submenu");
+			smenu.setAttribute("OnClick","submenu("+i+")");
+			getElm[i].appendChild(smenu);
+		};
+	};
+	submenu = function (i){
+		var sub = getElm[i].children[1];
+		var b = sub.getAttribute('style');
+		if(b){
+			sub.removeAttribute('style');
+			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-expand.png)';
+			getElm[i].lastChild.style.backgroundColor='rgba(98, 0, 49, 0.91)';
+		} else {
+			sub.style.display='block';
+			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-collapse.png)';
+			getElm[i].lastChild.style.backgroundColor='rgba(0, 0, 0, 0.91)';
+		}
+	};
+};
